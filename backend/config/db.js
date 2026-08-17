e9aef34c7a9e704751f7a2ea228c8d1f214888f9
PriyanshuKumar Sahu <priyansh_0201240057@jagannathuniversity.org>
2026-08-17 17:20:20 +0530

    Connect MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-
   try {
-
-    await mongoose.connect(
-      "mongodb://127.0.0.1:27017/ApnaBazar"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
-
-  } catch(error) {
-
+  } catch (error) {
     console.log("MongoDB Error:", error.message);
-
     process.exit(1);
-
   }
-
 };
-
 
 module.exports = connectDB;
